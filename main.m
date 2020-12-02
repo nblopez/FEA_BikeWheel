e88e6db9f9dc9d6407ffb6f8a090fedbb9f59547
nblopez <nblopez@illinois.edu>
2020-12-01 23:47:09 -0600

    Initial struct setup and formatting

---
+++ main.m
@@ -0,0 +1,69 @@
+%% main.m 
+% This file contains material and node parameters
+% as well as required set of functions to run analysis
+
+%% Spoke Parameters
+spoke = struct();
+spoke.material = 'Stainless Steel';
+spoke.rho = 7600; %kg/m^3
+spoke.E = 190 * 10^9; %Pa
+spoke.length = 0.33; %m (330mm)
+spoke.diameter = 0.002; %m (2mm)
+
+
+% Rim Parameters
+rim = struct();
+rim.material = 'Aluminum';
+rim.rho = 2700; %kg/m^3
+rim.E = 70 * 10^9; %Pa
+rim.diameter = spoke.length * 2; %m
+rim.thickness = 0.25; %m
+
+%% Modifiable Parameters
+spoke.count = 20; %System is impossible to solve with less than 3 spokes
+spoke.pattern = 'radial';
+% spoke.pattern = '1-cross';
+% spoke.pattern = '2-cross';
+
+rim.elem_count = spoke.count;
+
+[nodemap, glob_coord] = node_mapper(spoke, rim);
+% Row 1 of glob_coord is X, Row 2 of glob_coord is Y
+
+
+
+
+
+
+
+
+
+
+
+%% Deflection Calculation
+U = zeros(max(max(nodemap))*2, 1); %2DoF per node
+U(2) = 0.1;
+U(3) = 0.05;
+
+
+
+
+%% Graphical Output
+PlotStructure(glob_coord, U, nodemap)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
