a77355beadf7d05b4e4309b3745147da251b8b8f
nblopez <nblopez@illinois.edu>
2020-12-02 09:41:24 -0600

    Updated to have 3-node rim elements

--- node_mapper.m
+++ node_mapper.m
@@ -3,37 +3,38 @@ function [nodemap, glob_coord] = node_mapper(spoke, rim)
 % Node 1 is hub center
 % Node 2 is assumed to be directly below
 
-nodemap = zeros(spoke.count + rim.elem_count, 2);
+nodemap = zeros(spoke.count + rim.elem_count, 3);
+spoke_angle_spacing = 2 * pi / spoke.count;
+node_angle_spacing = spoke_angle_spacing / 2;
+
 
 switch spoke.pattern
     
     case 'radial'
-        for spoke_node = 2:spoke.count+1
-            nodemap(spoke_node-1, :) = [1, spoke_node];
+        for spoke_node = 1:spoke.count
+            % Every other node has spoke connected
+            nodemap(spoke_node, :) = [1, 2*spoke_node, 0];
+            % Rim element mapping
+            rim_elem_num = spoke_node + spoke.count;
+            nodemap(rim_elem_num, :) = [2*spoke_node - 1, 2*spoke_node, 2*spoke_node + 1];
         end
+        nodemap(spoke.count + 1,:) = [max(max(nodemap)), 2, 3]; 
         
-        start_rim_elem = (spoke.count + 1);
-        end_rim_elem = (spoke.count + rim.elem_count);
-        rim_diff = end_rim_elem - start_rim_elem + 1;
-        for rim_elem = start_rim_elem:end_rim_elem
-            nodemap(rim_elem, :) = [rim_elem - rim_diff + 1, rim_elem - rim_diff + 2];
-        end
-        
-        nodemap(end,:) = [rim.elem_count+1, 2];
         
         glob_coord = zeros(2, max(max(nodemap)));
-        
-        spoke_angle_spacing = 2 * pi / spoke.count;
-        for node = 2:spoke.count + 1
-            spoke_angle = (node - 2) * spoke_angle_spacing;
-            glob_coord(1, node) = spoke.length * sin(spoke_angle);
-            glob_coord(2, node) = -spoke.length * cos(spoke_angle);
+
+        for node = 2:length(glob_coord)
+            node_angle = node_angle_spacing * (node - 2); % 0 rad at node 2
+            X = spoke.length * sin(node_angle);
+            Y = -spoke.length * cos(node_angle);
+            
+            glob_coord(:, node) = [X; Y];
         end
         
         
-        
-        
+               
     case '1-cross'
+        
         
         
     case '2-cross'
