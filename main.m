12bef05e14a6f88fc7f2378169060b4ee87ea0bc
nblopez <nblopez@illinois.edu>
2020-12-08 20:23:08 -0600

    added spoke cross sectional area

--- main.m
+++ main.m
@@ -10,12 +10,11 @@ spoke.rho = 7600; %kg/m^3
 spoke.E = 190 * 10^9; %Pa
 spoke.length = 0.33; %m (330mm)
 spoke.diameter = 0.002; %m (2mm)
+spoke.Ae = pi * spoke.diameter^2 / 4; %m^2
 
 % Hub Parameters
 hub = struct();
 hub.material = 'Super Stiff Material';
-hub.rho = 0; %Weightless
-hub.E = 1e30; %Super stiff
 hub.diameter = 0.1;
 
 % Rim Parameters
@@ -28,11 +27,11 @@ rim.thickness = 0.25; %m
 
 
 %% Modifiable Parameters
-spoke.count = 36;
+spoke.count = 8;
 % spoke.pattern = 'radial'; %Minimum 3 spokes
-% spoke.pattern = '1-cross'; %Minimum 6 spokes (Even Number Only)
+spoke.pattern = '1-cross'; %Minimum 6 spokes (Even Number Only)
 % spoke.pattern = '2-cross'; %Minimum 12 spokes (Even Number Only)
-spoke.pattern = '3-cross'; %Minimum 18 spokes (Even Number Only)
+% spoke.pattern = '3-cross'; %Minimum 18 spokes (Even Number Only)
 % 3 Nodes per element on rim
 rim.elem_count = spoke.count;
 rim.node_count = 3 * rim.elem_count;
@@ -76,11 +75,12 @@ if strcmp(print_details, 'y')
     node_def = [[1:nnodes]', U(1:2:end) * 1e3, U(2:2:end) * 1e3];
     disp(node_def)
     
+    
     [max_def_row, ~] = ind2sub(size(node_def), find(node_def == max(max(node_def))));
     fprintf('Maximum deflection occurs at node: %d\nDeflections [mm]: X = %0.3f, Y = %0.3f\n', ...
         node_def(max_def_row, 1), node_def(max_def_row, 2), node_def(max_def_row, 3))
+    
 end
-
 
 PlotStructure(spoke, glob_coord, U, nodemap)
 
